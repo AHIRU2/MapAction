b35d350539d55a04ba670b32cae6ab698440f939
Shirahata <asuka.meiji@gmail.com>
2021-09-23 07:27:02 +0900

    no message

--- Assets/Scripts/EnemyDataSO.cs
+++ Assets/Scripts/EnemyDataSO.cs
@@ -13,11 +13,31 @@ public class EnemyDataSO : ScriptableObject
     {
         public int no; //番号
         public string name; //名前
-        public GameObject enemyPrefab; //敵キャラ
+        public EnemyController enemyPrefab; //敵キャラ
         public int attackPower; //攻撃力
         public float maxHp; //HP
         public float moveSpeed; //速さ
+        public int exp;
+        public float attackInterval;
 
+        //デバフ用のコンディションのデータ
+        public EnemyDebuffData[] debuffDatas;
+    }
+
+
+
+    /// <summary>
+    /// デバフ用のコンディションの登録用
+    /// </summary>
+    [System.Serializable]
+    public class EnemyDebuffData
+    {
+        //デバフ用のコンディションの設定
+        public ConditionType debuffconditionType;
+
+        //デバフ用のコンディションの付与確率
+        [Range(0, 100)]
+        public int rate;
     }
     
 
--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -12,6 +12,32 @@ public class GameData : MonoBehaviour
 
     public int maxHp;
 
+    //現在のレベル
+    public int playerLevel;
+
+    //経験値のトータル
+    public int totalExp;
+
+    //アビリティポイント
+    public int abilityPint;
+
+    //バトルで付与されたデバフのリスト
+    public List<ConditionType> debuffConditionList = new List<ConditionType>();
+
+    //選択しているステージの番号
+    public int chooseStageNo;
+
+    //クリア済みのステージの番号
+    public List<int> clearedStageNos;
+
+    // TODO 選択しているステージのデータ
+    //public StageData currentStageData;
+
+    //ボスバトルになったかどうかの確認用
+    public bool isBossBattled;
+
+    //public int attackPower;
+
     public int MaxMoveCount;
 
     public enum GameState
@@ -36,6 +62,25 @@ public class GameData : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    //ゲームの初期化
+    InitialzeGameData();
+
+    //ゲームの初期化用ローカル関数
+    void InitialzeGameData()
+    {
+        // TODO キャラ用のデータがある場合には、そのキャラごとの最大&#13259;を設定
+        //maxHp=currentCharaData.maxHp;
+
+        hp = maxHp;
+
+        playerLevel = 1;
+
+        totalExp = 0;
+
+        // TODO レベルアップで獲得できるボーナスのポイント
+        //abilityPoint+=playerLevel;
     }
 
 }
